9df5ff77effefe88a95321691c5a2dd7fe0d0eac
DiegoLeandro477 <diegoleandro477@gmail.com>
2023-05-25 01:04:55 -0300

    User update

--- src/main/java/com/example/curso/services/UserService.java
+++ src/main/java/com/example/curso/services/UserService.java
@@ -14,21 +14,33 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
-		return repository.findAll();	// Retorna todos os objs do BD
+		return repository.findAll(); // Retorna todos os objs do BD
 	}
-	
+
 	public User findById(Long id) {
-		Optional<User> obj = repository.findById(id);	// Retorna um obj Optional
-		return obj.get();		// Retorna o obj(User) do obj(Optional);
+		Optional<User> obj = repository.findById(id); // Retorna um obj Optional
+		return obj.get(); // Retorna o obj(User) do obj(Optional);
 	}
-	
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateDate(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateDate(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
